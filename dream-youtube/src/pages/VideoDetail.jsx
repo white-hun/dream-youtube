5b92af933fa9163fd41f5ecea5006dcdd5f2ad5e
white-hun <121800818+white-hun@users.noreply.github.com>
2023-04-12 18:01:46 +0900

    Update VideoDetail.jsx

--- dream-youtube/src/pages/VideoDetail.jsx
+++ dream-youtube/src/pages/VideoDetail.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useYoutubeApi } from "../context/YoutubeApiContext";
+import { useQuery } from "@tanstack/react-query";
 
 export default function VideoDetail() {
-  return <div>VideoDetail</div>;
+  const { videoId } = useParams();
+  const { keyword } = useParams();
+  const { youtube } = useYoutubeApi();
+  let {
+    isLoading,
+    error,
+    data: detail,
+  } = useQuery(["detail", keyword], () => youtube.search(keyword));
+  const videoDetail = detail?.find((item) => item.id === videoId);
+
+  return <>{videoDetail && console.log(videoDetail)}</>;
+  // return <div>{videoDetail.snippet.title}</div>;
 }
