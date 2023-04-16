19a6a082938c18d90db4d153a69e824d1e398c5e
white-hun <exwhite@naver.com>
2023-04-16 15:52:38 +0900

    Update date.js

--- src/util/date.js
+++ src/util/date.js
@@ -6,3 +6,7 @@ register("ko", koLocale);
 export function formatAgo(date, lang = "en_US") {
   return format(date, lang);
 }
+
+// 언어를 변경하고 싶으면 register를 사용한다
+// 사용하고 싶은 언어의 locale을 가지고 온다
+// 그 후 format 두번째 인자에 언어를 적어주면 된다
