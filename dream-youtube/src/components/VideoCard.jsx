a599c924cabc6c130ab0399967b2b6f6b12d858d
white-hun <121800818+white-hun@users.noreply.github.com>
2023-07-22 23:07:22 +0900

    Update

--- dream-youtube/src/components/VideoCard.jsx
+++ dream-youtube/src/components/VideoCard.jsx
@@ -11,13 +11,14 @@ export default function VideoCard({ video, type }) {
   };
   const navigate = useNavigate();
   const handleClick = () => {
-    navigate(`/videos/watch/${video.id}`, { state: { video } }); // MY: navigate 두번째 인자에 객체를 전달할 수 있다 state라는 key를 사용한다
+    navigate(`/videos/watch/${video.id}`, { state: { video } });
   };
+  // MY: navigate 두번째 인자에 객체를 전달할 수 있다 state라는 key를 사용한다
   const isList = type === "list";
 
   return (
     <li className={isList ? "flex gap-1 m-2" : ""} onClick={handleClick}>
-      <img className={isList ? "w-60 mr-2" : "w-full"} src={thumbnails.high.url} alt={title} />
+      <img className={isList ? "w-60 mr-2" : "w-full"} src={thumbnails.medium.url} alt={title} />
       <div className="">
         <p className="font-semibold my-2 line-clamp-2">{decodeHtml(title)}</p>
         <p className="text-sm opacity-80">{channelTitle}</p>
--- dream-youtube/src/components/test/VideoCard.test.js
+++ dream-youtube/src/components/test/VideoCard.test.js
@@ -1,3 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { MemoryRouter, Route, Routes, useLocation } from "react-router-dom";
+import { formatAgo } from "../../util/date";
+import VideoCard from "../VideoCard";
+
 describe("VideoCard", () => {
   const video = {
     id: 1,
@@ -5,7 +11,7 @@ describe("VideoCard", () => {
       title: "title",
       channelId: "1",
       channelTitle: "channelTitle",
-      publidhedAt: newDate(),
+      publishedAt: new Date(),
       thumbnails: {
         medium: {
           url: "http://image/",
@@ -13,7 +19,44 @@ describe("VideoCard", () => {
       },
     },
   };
+  const { title, channelTitle, publishedAt, thumbnails } = video.snippet;
+
   it("renders video item", () => {
-    render(<VideoCard video={video} />);
+    render(
+      <MemoryRouter>
+        <VideoCard video={video} />
+      </MemoryRouter>
+    );
+
+    const image = screen.getByRole("img");
+    expect(image.src).toBe(thumbnails.medium.url);
+    expect(image.alt).toBe(title);
+    expect(screen.getByText(title)).toBeInTheDocument();
+    expect(screen.getByText(channelTitle)).toBeInTheDocument();
+    expect(screen.getByText(formatAgo(publishedAt))).toBeInTheDocument();
+  });
+
+  it("navigates to detailed video page with video state when clicked", async () => {
+    function LocationStateDisplay() {
+      return <pre>{JSON.stringify(useLocation().state)}</pre>;
+    }
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Routes>
+          <Route path="/" element={<VideoCard video={video} />} />
+          <Route path={`/videos/watch/${video.id}`} element={<LocationStateDisplay />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const card = screen.getByRole("listitem");
+    await userEvent.click(card);
+
+    expect(screen.getByText(JSON.stringify({ video }))).toBeInTheDocument();
   });
 });
+
+// useNavigate() may be used only in the context of a <Router> component.
+// VideoCard 컴포넌트에서 useNavigate라는 react-router를 사용하고 있다
+// react-router 컴포넌트를 만들 때에는 react-router 환경을 만들어 줘야한다
+// <MemoryRouter>로 컴포넌트를 감싸준다(<StaticRouter>, <MemoryRouter>, <BrowserRouter>)
