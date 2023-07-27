89612307242f9c73e411bc9227d2ea03efd51c8b
white-hun <121800818+white-hun@users.noreply.github.com>
2023-07-27 23:30:57 +0900

    Update VideoCard.test.js

--- dream-youtube/src/components/tests/VideoCard.test.js
+++ dream-youtube/src/components/tests/VideoCard.test.js
@@ -74,47 +74,47 @@
 //------------------------------------------------------------------------------------------------------
 // 리펙토링
 
-import { render, screen, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import { Route, useLocation } from "react-router-dom";
-import { withRouter } from "../../tests/utils";
-import { fakeVideo as video } from "../../tests/videos";
-// videos.js에서 만든 fakeVideo를 가져와서 video라는 이름으로 사용한다
-import { formatAgo } from "../../util/date";
-import VideoCard from "../VideoCard";
+// import { render, screen, waitFor } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+// import { Route, useLocation } from "react-router-dom";
+// import { withRouter } from "../../tests/utils";
+// import { fakeVideo as video } from "../../tests/videos";
+// // videos.js에서 만든 fakeVideo를 가져와서 video라는 이름으로 사용한다
+// import { formatAgo } from "../../util/date";
+// import VideoCard from "../VideoCard";
 
-describe("VideoCard", () => {
-  const { title, channelTitle, publishedAt, thumbnails } = video.snippet;
+// describe("VideoCard", () => {
+//   const { title, channelTitle, publishedAt, thumbnails } = video.snippet;
 
-  it("renders video item", () => {
-    render(withRouter(<Route path="/" element={<VideoCard video={video} />} />));
+//   it("renders video item", () => {
+//     render(withRouter(<Route path="/" element={<VideoCard video={video} />} />));
 
-    const image = screen.getByRole("img");
-    expect(image.src).toBe(thumbnails.medium.url);
-    expect(image.alt).toBe(title);
-    expect(screen.getByText(title)).toBeInTheDocument();
-    expect(screen.getByText(channelTitle)).toBeInTheDocument();
-    expect(screen.getByText(formatAgo(publishedAt))).toBeInTheDocument();
-  });
+//     const image = screen.getByRole("img");
+//     expect(image.src).toBe(thumbnails.medium.url);
+//     expect(image.alt).toBe(title);
+//     expect(screen.getByText(title)).toBeInTheDocument();
+//     expect(screen.getByText(channelTitle)).toBeInTheDocument();
+//     expect(screen.getByText(formatAgo(publishedAt))).toBeInTheDocument();
+//   });
 
-  it("navigates to detailed video page with video state when clicked", async () => {
-    function LocationStateDisplay() {
-      return <pre>{JSON.stringify(useLocation().state)}</pre>;
-    }
-    render(
-      withRouter(
-        <>
-          <Route path="/" element={<VideoCard video={video} />} />
-          <Route path={`/videos/watch/${video.id}`} element={<LocationStateDisplay />} />
-        </>
-      )
-    );
+//   it("navigates to detailed video page with video state when clicked", async () => {
+//     function LocationStateDisplay() {
+//       return <pre>{JSON.stringify(useLocation().state)}</pre>;
+//     }
+//     render(
+//       withRouter(
+//         <>
+//           <Route path="/" element={<VideoCard video={video} />} />
+//           <Route path={`/videos/watch/${video.id}`} element={<LocationStateDisplay />} />
+//         </>
+//       )
+//     );
 
-    const card = screen.getByRole("listitem");
-    userEvent.click(card);
+//     const card = screen.getByRole("listitem");
+//     userEvent.click(card);
 
-    await waitFor(() => {
-      expect(screen.getByText(JSON.stringify({ video }))).toBeInTheDocument();
-    });
-  });
-});
+//     await waitFor(() => {
+//       expect(screen.getByText(JSON.stringify({ video }))).toBeInTheDocument();
+//     });
+//   });
+// });
