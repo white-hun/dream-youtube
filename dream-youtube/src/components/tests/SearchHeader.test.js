eb0a7bdfcbc763d74fce2cdecb422a441df792b4
white-hun <121800818+white-hun@users.noreply.github.com>
2023-08-01 22:12:29 +0900

    Update

--- dream-youtube/src/components/tests/SearchHeader.test.js
+++ dream-youtube/src/components/tests/SearchHeader.test.js
@@ -1,3 +1,7 @@
 describe("SearchHeader", () => {
   it("", () => {});
 });
+
+// input 의 value를 받아서 setText에 useState로 저장
+// 저장한 text를 keyword로 설정(없으면 빈 문자열, keyword가 바뀔 때 마다 실행)
+// 제출되면 keyword(text)를 포함한 경로로 navigate
