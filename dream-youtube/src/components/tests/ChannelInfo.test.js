1dab1fd5af4375cf9f8b9aba1f671bb5ef81c7c1
white-hun <121800818+white-hun@users.noreply.github.com>
2023-09-16 22:40:35 +0900

    Update

--- dream-youtube/src/components/tests/ChannelInfo.test.js
+++ dream-youtube/src/components/tests/ChannelInfo.test.js
@@ -23,18 +23,46 @@ describe("ChannelInfo", () => {
     // 성공적인 케이스를 text하기 위해 호출되면 url을 return
     fakeYoutube.channelImageURL.mockImplementation(() => "url");
     // 컴포넌트를 전달할 때는 Route로 감싸야한다
-    render(
+    const { asFragment } = render(
       withAllContexts(
         withRouter(<Route path="/" element={<ChannelInfo id="id" name="channel" />} />), // 1번째 prop(children)
         fakeYoutube // 2번째 prop(youtube)
       )
     );
-
     // waitfor 주어진 함수가 에러를 던지지 않을 때 까지 재시도한다(요소를 화면에서 찾을 때 까지 재시도를 한다(기다린다))
     await waitFor(() => {
-      screen.getByText("channel");
+      screen.getByRole("img");
+    });
+    expect(asFragment()).toMatchSnapshot();
+  });
+  // 그냥 snapshot 하면 url 받아오는 것이 비동기이기 때문에 image가 없이(url없이) name만 출력된다
+  // img가 나타날 때까지 기다린다, mock 구현사항에서 url을 반환하니까 channelIndo 컴포넌트에서 url을 받아오면 img가 생성되니까 img를 나타낼 수 있다
+  // 특정 사용자 작업에 따라 변경되는 테스트가 있을 수 있는데 이런 변동성이 있는 값들을 rendeResult로써 렌더링된 컴포넌트를 반환하기 위해 asFragment를 사용한다
+
+  it("renders without URL", () => {
+    renderChannelInfoWithCallback(() => {
+      throw new Error("error");
+    });
+    expect(screen.queryByRole("img")).toBeNull();
+  });
+
+  it("renders with URL", async () => {
+    renderChannelInfoWithCallback("url");
+
+    await waitFor(() => {
+      expect(screen.getByRole("img")).toBeInTheDocument();
     });
   });
+
+  function renderChannelInfoWithCallback(callback) {
+    fakeYoutube.channelImageURL.mockImplementation(() => callback);
+    return render(
+      withAllContexts(
+        withRouter(<Route path="/" element={<ChannelInfo id="id" name="channel" />} />),
+        fakeYoutube
+      )
+    );
+  }
 });
 
 // axios를 test 라이브러리에서 사용하면 항상 에러가 발생한다(SyntaxError: Cannot use import statement outside a module)
