80306619c847bebf418c53372d9900a7e1570f42
white-hun <121800818+white-hun@users.noreply.github.com>
2023-10-18 22:18:03 +0900

    Update

--- dream-youtube/src/pages/tests/NotFound.test.js
+++ dream-youtube/src/pages/tests/NotFound.test.js
@@ -9,3 +9,8 @@ describe("NotFound", () => {
     expect(component.toJSON()).toMatchSnapshot();
   });
 });
+
+// 메모리 라우터를 만들어서 라우터를 설정한다음에 정의한 경로 외에 다른 곳으로 가면
+// <NotFound/>가 나오는지 테스트를 해봐도 되지만
+// 그럼 우리의 코드를 테스트하는 것이 아니라 React Router 자체를 테스트하는 것이기 때문에 굳이 그정도 수준의 테스트는 작성하지 않는다
+// 이미 React Router 라이브러리 자체에 완벽한 TDD가 작성되어 검증되어 있기 때문이다
--- dream-youtube/src/pages/tests/Videos.test.js
+++ dream-youtube/src/pages/tests/Videos.test.js
@@ -1,55 +1,77 @@
 import { Route } from "react-router-dom";
 import { withAllContexts, withRouter } from "../../tests/utils";
-import { fakeVideos } from "../../tests/videos";
+import { fakeVideo, fakeVideos } from "../../tests/videos";
 import { render, screen, waitFor, waitForElementToBeRemoved } from "@testing-library/react";
 import Videos from "../Videos";
 
-describe("Videos", () => {
-  const keyword = "keyword";
+describe("Videos component", () => {
   const fakeYoutube = {
     search: jest.fn(),
   };
 
+  beforeEach(() => {
+    fakeYoutube.search.mockImplementation((keyword) => {
+      return keyword ? [fakeVideo] : fakeVideos;
+    });
+  });
+
   afterEach(() => fakeYoutube.search.mockReset());
 
-  it("render correctly", async () => {
-    fakeYoutube.search.mockImplementation(() => fakeVideos);
-    const { asFragment } = renderVideos();
+  // it("render correctly", async () => {
+  //   fakeYoutube.search.mockImplementation(() => fakeVideos);
+  //   const { asFragment } = renderVideos();
 
-    await waitForElementToBeRemoved(() => expect(screen.getByText("Loading...")));
-    expect(asFragment()).toMatchSnapshot();
+  //   await waitForElementToBeRemoved(() => expect(screen.getByText("Loading...")));
+  //   expect(asFragment()).toMatchSnapshot();
+  // });
+
+  it("renders all videos when keyword is not specified", async () => {
+    renderWithPath("/"); // keyword 없이 최상위 경로로 이동하면
+
+    expect(fakeYoutube.search).toHaveBeenCalledWith(undefined); // 아무런 인자가 없는 상태로 호출되어야하고
+
+    await waitFor(() => expect(screen.getAllByRole("listitem")).toHaveLength(fakeVideos.length));
   });
 
-  it("render related videos correctly", async () => {
-    fakeYoutube.search.mockImplementation(() => fakeVideos);
-    renderVideos();
+  it("when keyword is specified, renders search results", async () => {
+    const searchKeyword = "fake-keyword";
+    renderWithPath(`/${searchKeyword}`); // keyword가 있는 경로로 렌더링 했을 경우
 
-    expect(fakeYoutube.search).toBeCalledWith("keyword");
-    // await waitFor(() => expect(screen.getAllByRole("listitem")).toHaveLength(fakeVideos.length));
+    expect(fakeYoutube.search).toHaveBeenCalledWith(searchKeyword); // keyword와 함께 호출
+    await waitFor(() => {
+      expect(screen.getAllByRole("listitem")).toHaveLength(1);
+    });
   });
 
-  it("render loading", () => {
-    fakeYoutube.search.mockImplementation(() => fakeVideos);
-    renderVideos();
+  it("renders loading state when items are being fetched", async () => {
+    renderWithPath("/");
 
-    expect(screen.getByText("Loading...")).toBeInTheDocument();
+    expect(screen.getByText(/Loading.../i)).toBeInTheDocument();
   });
 
-  it("render error", async () => {
-    fakeYoutube.search.mockImplementation(() => {
+  it("renders error state when fetching items fails", async () => {
+    fakeYoutube.search.mockImplementation(async () => {
       throw new Error("error");
     });
-    renderVideos();
+
+    renderWithPath("/");
 
     await waitFor(() => {
-      expect(screen.getByText("Something is wrong.")).toBeInTheDocument();
+      expect(screen.getByText(/Something is wrong/i)).toBeInTheDocument();
     });
   });
 
-  function renderVideos() {
+  function renderWithPath(path) {
     return render(
       withAllContexts(
-        withRouter(<Route path="/videos/:keyword" element={<Videos />} />, "/videos/:keyword")
+        withRouter(
+          <>
+            <Route path="/:keyword" element={<Videos />} />
+            <Route path="/:keyword" element={<Videos />} />, ))
+          </>,
+          path
+        ),
+        fakeYoutube
       )
     );
   }
